5f789528b4f5e48f2bed44fcd0829bfd39d2a7dc
louisbl <louis@hyperfiction.fr>
2013-04-12 10:31:39 +0200

    Add inthebox signals in src. Update readme.

--- readme.md
+++ readme.md
@@ -3,12 +3,18 @@ HypPusher
 A native extension to add Pusher to your NME application
 ---------------------------------------------------------
 
-See <a href="http://pusher.com/docs">pusher documentation</a>
-Use <a href="https://github.com/jmschultz/JavaPusherClient">JavaPusherClient</a> for Android
-Use <a href="https://github.com/lukeredpath/libPusher">LibPusher</a> for iOS
+See the <a href="http://pusher.com/docs">pusher documentation</a>.
+
+Use <a href="https://github.com/jmschultz/JavaPusherClient">JavaPusherClient</a> for Android.
+
+Use <a href="https://github.com/lukeredpath/libPusher">LibPusher</a> for iOS.
+
 Default is to use wss://, on port 443 as it works better on mobile connection
 In order to use private channel, you need to provide an end point url to authenticate
-Limitations : only one channel at a time for now, no presence channel
+
+Limitations
+-----------
+No presence channel and no tests for multiple channels (it may work).
 
 Installation
 ------------
@@ -21,8 +27,77 @@ There is an [include.nmml](https://github.com/hyperfiction/HypPusher/blob/master
 
 Usage
 -----
+Initialize a pusher client, without autoconnect. It needs your api key and for private channel channel your third party server url with an optional token and optional userid for authentication.
 
-TODO
+	hp = new HypPusher( api_key, server_url, user_id );
+
+This extension uses signals:
+
+	public var onConnect       	: Signal1<String>;
+	public var onConnectError  	: Signal;
+	public var onDisconnect    	: Signal;
+	public var onSubscribed    	: Signal1<String>;
+	public var onSubscribeError	: Signal2<String,String>;
+	public var onMessage       	: Signal3<String, Dynamic, String >;
+	public var onChannelMessage	: Signal3<String, Dynamic, String >;
+
+To connect to the pusher server and get a socket id:
+
+	hp.onConnect.connect( _onConnect );
+	hp.onConnectError.connect( _onConnectError );
+	hp.connectToServer( );
+
+	function _onConnect( socket_id : String ){
+		trace( "pusher connected with socket id: "+socket_id );
+		trace( "pusher is connected? "+hp.is_connected );
+		trace( "pusher socket id: "+hp.socket_id );
+	}
+
+	function _onConnectError( ) {
+		trace( "cannot connect to pusher!" );
+	}
+
+To connect to a public/private channel:
+If it's a private channel it trigger the authentication mecanism.
+
+	hp.onSubscribed.connect( _onSubscribed );
+	hp.setChannel( "channel_name" );
+	// hp.setChannel( "private-channel_name" );
+
+	function _onSubscribed( channel_name : String ) {
+		trace( 'connected to channel: '+channel_name );
+	}
+
+By default the android version listens to events on all channels. For iOS you should use:
+
+	hp.onMessage.connect( _onMessage );
+	hp.bind( "event_name" );
+
+	function _onMessage( event_name : String, data : Dynamic, channel_name : String ) {
+		trace( Std.format( "on message: $event_name,
+					data: $data, channel_name: $channel_name" ) );
+	}
+
+To subscribe to message on a specific channel:
+
+	hp.onChannelMessage.connect( _onChannelMessage );
+	hp.bindOnChannel( "event_name", "channel_name" );
+
+	function _onChannelMessage( event_name : String, data : Dynamic, channel_name : String ) {
+		trace( Std.format( "on channel message: $event_name,
+					data: $data, channel_name: $channel_name" ) );
+	}
+
+Once you're subscribed to a private channel, you can send message:
+
+	hp.send( "event-name", {"optional":"json data"}, "channel_name" );
+
+To unbind and unsubscribe:
+
+	hp.unbind( "event_name" );
+	hp.unbindOnChannel( "event_name", "channel_name" );
+	hp.unsetChannel( "channel_name" );
+
 
 Made at [Hyperfiction](http://hyperfiction.fr)
 ----------------------------------------------
---
+++ src/org/shoebox/utils/system/ASignal.hx
@@ -0,0 +1,215 @@
+/**
+*  HomeMade by shoe[box]
+*
+*  Redistribution and use in source and binary forms, with or without 
+*  modification, are permitted provided that the following conditions are
+*  met:
+*
+* Redistributions of source code must retain the above copyright notice, 
+*   this list of conditions and the following disclaimer.
+*  
+* Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the 
+*    documentation and/or other materials provided with the distribution.
+*  
+* Neither the name of shoe[box] nor the names of its 
+* contributors may be used to endorse or promote products derived from 
+* this software without specific prior written permission.
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+* IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+* PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.shoebox.utils.system;
+
+import org.shoebox.collections.PriorityQueue;
+
+/**
+ * ...
+ * @author shoe[box]
+ */
+
+class ASignal<T>{
+
+	public var enabled( default , _setEnabled ) : Bool;
+
+	private var _oQueue : PriorityQueue<SignalListener<T>>;
+
+	// -------o constructor
+		
+		/**
+		* constructor
+		*
+		* @param	
+		* @return	void
+		*/
+		public function new( ) {
+			_oQueue = new PriorityQueue<SignalListener<T>>( );
+			enabled = true;
+		}
+	
+	// -------o public
+		
+		/**
+		* 
+		* 
+		* @public
+		* @return	void
+		*/
+		public function connect( f : T , iPrio : Int = 0 , iCount : Int = -1 ) : Void {
+			
+			if( _exist( f , iPrio ) )
+				return;
+
+			_oQueue.add( new SignalListener( f , iCount , iPrio ) , iPrio );
+
+		}
+
+		/**
+		* 
+		* 
+		* @public
+		* @return	void
+		*/
+		public function disconnect( f : T , prio : Int = 0 ) : Void {
+		
+			var content = _oQueue.getContent( );
+			for( o in content ){
+
+				if( o.content.fListener == f && o.prio == prio ){
+					content.remove( o );
+					_oQueue.remove( o.content );
+					break;
+				}
+			}
+
+		}
+
+		/**
+		* 
+		* 
+		* @public
+		* @return	void
+		*/
+		public function dispose( ) : Void {
+			_oQueue = null;
+		}
+
+		/**
+		* 
+		* 
+		* @public
+		* @return	void
+		*/
+		public function hasListener( ) : Bool {
+			return _oQueue.length > 0;
+		}
+
+		/**
+		* 
+		* 
+		* @public
+		* @return	void
+		*/
+		public function exists( f : T , prio : Int = 0 ) : Bool {
+			return _exist( f , prio );		
+		}
+
+	// -------o protected
+		
+		/**
+		* 
+		* 
+		* @private
+		* @return	void
+		*/
+		private function _exist( f : T , prio : Int = 0 ) : Bool{
+
+			var b : Bool = false;
+			var content = _oQueue.getContent( );
+			for( o in content ){
+				if( o.content.fListener == f && o.prio == prio ){
+					b = true;
+					break;
+				}
+			}
+			return b;
+
+		}
+
+		/**
+		* 
+		* 
+		* @private
+		* @return	void
+		*/
+		private function _check( l : SignalListener<T> ) : Void{
+
+			if( l.iCount != -1 ){
+				l.iCount--;
+			}
+
+			if( l.iCount == 0 )
+				disconnect( l.fListener , l.iPrio );
+			
+		}	
+
+		/**
+		* 
+		* 
+		* @private
+		* @return	void
+		*/
+		private function _setEnabled( b : Bool ) : Bool{
+			this.enabled = b;
+			return b;
+		}
+
+	// -------o misc
+	
+}
+
+
+/**
+ * ...
+ * @author shoe[box]
+ */
+
+class SignalListener<T>{
+
+	public var fListener : T;
+	public var iCount    : Int;
+	public var iPrio     : Int;
+
+	// -------o constructor
+		
+		/**
+		* constructor
+		*
+		* @param	
+		* @return	void
+		*/
+		public function new( f : T , iCount : Int , iPrio : Int ) {
+			this.fListener = f;
+			this.iCount    = iCount;
+			this.iPrio     = iPrio;
+		}
+	
+	// -------o public
+				
+				
+
+	// -------o protected
+	
+		
+
+	// -------o misc
+	
+}
